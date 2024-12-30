cb20584c9dcf640c755738cbebfbc94d58a63e3b
strr0 <38580170+strr0@users.noreply.github.com>
2024-12-31 00:11:40 +0800

    feat: spring example liquibase

---
+++ spring-example-liquibase/src/test/java/com/strr/liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,10 @@
+package com.strr.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+    @Test
+    void test() {}
+}
